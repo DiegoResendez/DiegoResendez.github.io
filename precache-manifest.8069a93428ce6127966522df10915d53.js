bd0bfdf31263e115ff2b7b6033a2fe8f105b82a1
unknown <diego.resendez@yahoo.com>
2020-09-17 15:26:53 -0700

    Updates

---
+++ precache-manifest.8069a93428ce6127966522df10915d53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eae2fbc3050a2935f0f00743ab0fce5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f96df3c8d02477ae3cef",
+    "url": "/static/css/main.cab9c331.chunk.css"
+  },
+  {
+    "revision": "9128b23813fc76d57bfa",
+    "url": "/static/js/2.9457c7cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9457c7cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f96df3c8d02477ae3cef",
+    "url": "/static/js/main.34b3fe9f.chunk.js"
+  },
+  {
+    "revision": "dd0ad302ca727c93b192",
+    "url": "/static/js/runtime-main.4e7c19c6.js"
+  }
+]);
